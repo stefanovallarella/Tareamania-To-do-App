a5e0cfec0dd98a2e5f13b39def611da2ac1ccac3
stefanovallarella <stefanovallarella@gmail.com>
2020-11-23 17:03:32 -0300

    Se inicia proyecto funcionando en Nodejs/Express con Reactjs

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware nivel aplicación
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// HTTP Requests
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Get /api/hello desde Express' });
+});
+
+app.post('/api/world', (req, res) => {
+  console.log(req.body);
+  res.send(
+    `Llegó el req por POST. Data enviada por POST: ${req.body.post}`,
+  );
+});
+
+
+
+// Server 
+app.listen(port, () => console.log(`Listening on port ${port}`));
